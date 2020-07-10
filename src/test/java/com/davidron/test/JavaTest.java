c8d96e197ac51d5abf11425910e161ec17ca8989
David Ron <dron@payjunction.com>
2020-07-10 16:58:18 -0700

    Github Actions Test, Gradle Build, and Github Workflow yml file.

---
+++ src/test/java/com/davidron/test/JavaTest.java
@@ -0,0 +1,16 @@
+package com.davidron.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author dron
+ * @since 7/10/20
+ */
+public class JavaTest {
+    @Test
+    public void testTest(){
+        assertEquals(1,1);
+    }
+}
